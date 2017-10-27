928b56539f0530b1837e1bfe293c90ce734fd97b
Christine Gamble <christine.gamble@nrel.gov>
2017-10-27 10:21:00 -0600

    Changes size for selected and disabled farm icons too

--- src/modules/map/Map.jsx
+++ src/modules/map/Map.jsx
@@ -255,7 +255,7 @@ export class Map extends React.Component {
         source: 'windfarms',
         layout: {
           'icon-image': 'windfarm-selected',
-          'icon-size': .24,
+          'icon-size': .4,
           'icon-allow-overlap': true,
           'icon-keep-upright': true
         },
@@ -276,7 +276,7 @@ export class Map extends React.Component {
         source: 'windfarms',
         layout: {
           'icon-image': 'windfarm-disabled',
-          'icon-size': .18,
+          'icon-size': .3,
           'icon-allow-overlap': true,
           'icon-keep-upright': true
         },
