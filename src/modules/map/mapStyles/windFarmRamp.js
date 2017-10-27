e01b3669867cc501782d51862b8f7ad24ad16d93
Christine Gamble <christine.gamble@nrel.gov>
2017-10-27 09:57:32 -0600

    Removes blinking for alerts

--- src/modules/map/mapStyles/windFarmRamp.js
+++ src/modules/map/mapStyles/windFarmRamp.js
@@ -52,10 +52,9 @@ export const initializeStyle = (map, layerSource) => {
     type: 'circle',
     source: 'windfarms',
     paint: {
-      'circle-radius': initialRadius,
+      'circle-radius': initialRadius+8,
       'circle-color': '#B00',
-      "circle-radius-transition": {duration: 0},
-      "circle-opacity-transition": {duration: 0},
+      'circle-opacity': 0.8
     },
     filter: [">=", "maxRampSeverity", 2],
     layout: {
@@ -65,41 +64,40 @@ export const initializeStyle = (map, layerSource) => {
   layerIds.push('windfarms-r-halo');
 }
 
-const toggleAnimation = (map, visibility) => {
-  if(visibility === 'none') {
-    animate = false;
-  } else {
-    // This function causes the red halo to blip
-    function animateMarker(timestamp) {
-      setTimeout(function(){
-        if(animate) {
-          requestAnimationFrame(animateMarker);
-
-          radius += (maxRadius - radius) / framesPerSecond;
-          opacity -= ( .9 / framesPerSecond );
-          opacity = opacity < 0 ? 0 : opacity;
-
-          map.setPaintProperty('windfarms-r-halo', 'circle-radius', radius);
-          map.setPaintProperty('windfarms-r-halo', 'circle-opacity', opacity);
-
-          if (opacity === 0) {
-            radius = initialRadius;
-            opacity = initialOpacity;
-          }
-        }
-      }, 1000 / framesPerSecond);
-    }
-
-    // Start the animation.
-    animate = true;
-    animateMarker(0);
-  }
-}
+// const toggleAnimation = (map, visibility) => {
+//   if(visibility === 'none') {
+//     animate = false;
+//   } else {
+//     // This function causes the red halo to blip
+//     function animateMarker(timestamp) {
+//       setTimeout(function(){
+//         if(animate) {
+//           requestAnimationFrame(animateMarker);
+//
+//           radius += (maxRadius - radius) / framesPerSecond;
+//           opacity -= ( .9 / framesPerSecond );
+//           opacity = opacity < 0 ? 0 : opacity;
+//
+//           map.setPaintProperty('windfarms-r-halo', 'circle-radius', radius);
+//           map.setPaintProperty('windfarms-r-halo', 'circle-opacity', opacity);
+//
+//           if (opacity === 0) {
+//             radius = initialRadius;
+//             opacity = initialOpacity;
+//           }
+//         }
+//       }, 1000 / framesPerSecond);
+//     }
+//
+//     // Start the animation.
+//     animate = true;
+//     animateMarker(0);
+//   }
+// }
 
 export const toggleVisibility = (map) => {
   if(layerIds.length === 0) { return; }
   const visible = map.getLayoutProperty(layerIds[0], 'visibility') === 'visible' ? 'none' : 'visible';
-  toggleAnimation(map, visible);
   layerIds.forEach(function(id){
     map.setLayoutProperty(id, 'visibility', visible);
   });
