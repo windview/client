b59f3069c38ccd9c238185155743c9c3a9c63551
Paul Edwards <pjfingers@gmail.com>
2018-02-22 11:10:06 -0500

    Clean up lint

--- src/modules/loadingBar/LoadingBar.jsx
+++ src/modules/loadingBar/LoadingBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './LoadingBar.scss';
-import {mapStateToProps, mapDispatchToProps} from './selectors';
+import {mapStateToProps} from './selectors';
 
 class LoadingBar extends React.Component {
 
--- src/modules/loadingBar/selectors.js
+++ src/modules/loadingBar/selectors.js
@@ -1,4 +1,3 @@
-import * as actions from '../../actionCreators';
 
 export const mapStateToProps = (state, ownProps) => {
   return {
