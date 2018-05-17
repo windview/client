7061960efb5c049d96ed80c4e18c15b7d7c569b5
Paul Edwards <pjfingers@gmail.com>
2018-05-17 10:53:36 -0400

    Adds auto-fetch of forecast every 5 minutes

--- src/modules/app/App.jsx
+++ src/modules/app/App.jsx
@@ -10,45 +10,58 @@ import AppSettings from '../appSettings/AppSettings';
 import Help from '../help/Help';
 import LoadingBar from '../loadingBar/LoadingBar';
 import WindFarm from '../../data/windFarm';
-import { mapStateToProps } from './selectors';
+import { mapStateToProps, mapDispatchToProps } from './selectors';
 import { connect } from 'react-redux';
 import { NAV_BAR_BUTTONS } from './constants';
 import classNames from 'classnames';
 import './App.scss';
 
-export const App = ({ activePane, selectedFeature }) => {
+class App extends React.Component {
 
-  let farmName = selectedFeature ? WindFarm.getWindFarmById(selectedFeature).name : null;
+  componentDidMount() {
+    let fetchForecasts = this.props.fetchForecasts,
+        fiveMinutes = 1000*60*5;
 
-  return (
-    <div id="app" className="">
-      <NavBar appTitle="Wind View" buttons={NAV_BAR_BUTTONS} />
-      <div className='main-content-container'>
-      <LoadingBar />
-        <div className={classNames({'hidden': activePane!=='map-view'})}>
-          <section id="map" className="main-pane-left">
-            <Map />
-          </section>
-          <section id="map-sidebar" className="main-pane-right">
-            <div className='detail-title'>{farmName}</div>
-            <ForecastChart />
-            <FarmDetail />
-            <ForecastMeta />
-          </section>
-          <section id="map-footer" className="main-pane-bottom">
-            <Slider />
-            <BotChartSelector />
-          </section>
-        </div>
-        <div className={classNames({'hidden': activePane!=='settings'})}>
-          <AppSettings />
-        </div>
-        <div className={classNames({'hidden': activePane!=='help'})}>
-          <Help />
+    setInterval(()=> {
+      fetchForecasts(WindFarm.getFarms());
+    }, (fiveMinutes));
+  }
+
+  render() {
+    let selectedFeature = this.props.selectedFeature,
+        activePane = this.props.activePane,
+        farmName = selectedFeature ? WindFarm.getWindFarmById(selectedFeature).name : null;
+
+    return (
+      <div id="app" className="">
+        <NavBar appTitle="Wind View" buttons={NAV_BAR_BUTTONS} />
+        <div className='main-content-container'>
+        <LoadingBar />
+          <div className={classNames({'hidden': activePane!=='map-view'})}>
+            <section id="map" className="main-pane-left">
+              <Map />
+            </section>
+            <section id="map-sidebar" className="main-pane-right">
+              <div className='detail-title'>{farmName}</div>
+              <ForecastChart />
+              <FarmDetail />
+              <ForecastMeta />
+            </section>
+            <section id="map-footer" className="main-pane-bottom">
+              <Slider />
+              <BotChartSelector />
+            </section>
+          </div>
+          <div className={classNames({'hidden': activePane!=='settings'})}>
+            <AppSettings />
+          </div>
+          <div className={classNames({'hidden': activePane!=='help'})}>
+            <Help />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default connect( mapStateToProps, null)(App);
+export default connect( mapStateToProps, mapDispatchToProps )(App);
--- src/modules/app/selectors.js
+++ src/modules/app/selectors.js
@@ -1,8 +1,21 @@
 // app/selectors
 
+import {
+  fetchForecast
+} from '../../actionCreators';
+
+
 export const mapStateToProps = (state, ownProps) => {
   return {
     activePane: state.nav.activePane,
     selectedFeature: state.analysis.selectedFeature
+  }
+}
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchForecasts: (windFarms) => {
+      dispatch(fetchForecast(windFarms));
+    }
   }
 }
